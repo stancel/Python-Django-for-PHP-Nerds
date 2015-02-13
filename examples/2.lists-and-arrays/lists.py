2dea7a524ea724c3f3b4d5e011910a555056444c
Daniel Quinn <dquinn@ripe.net>
2015-02-13 17:42:26 +0100

    Still figuring out how to break this tutorial up into understandable chunks

---
+++ examples/2.lists-and-arrays/lists.py
@@ -0,0 +1,12 @@
+# Lists are just like PHP's indexed arrays
+breakfast = ["eggs", "spam", "bacon", "pancakes", "oatmeal", "yogurt", "toast"]
+
+breakfast[0]  # eggs
+breakfast[2]  # bacon
+
+# But lists in Python are *much* more featureful.  For example, using our
+# breakfast list above:
+
+breakfast[-1]   # toast
+breakfast[2:4]  # ["bacon", "pancakes", "oatmeal"]
+
