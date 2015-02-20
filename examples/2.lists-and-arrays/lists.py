f06d83c122fc4d7253e80ab138d4b5a7be0e748c
Daniel Quinn <dquinn@ripe.net>
2015-02-20 17:20:56 +0100

    Added concatenation

--- examples/2.lists-and-arrays/lists.py
+++ examples/2.lists-and-arrays/lists.py
@@ -7,7 +7,15 @@ breakfast[2]  # bacon
 # But lists in Python are *much* more featureful.  For example, using our
 # breakfast list above:
 
-breakfast[-1]   # toast
-breakfast[2:5]  # ["bacon", "pancakes", "oatmeal"]
-breakfast[-2:]  # ["yogurt", "toast"]
+breakfast[-1]              # toast
+breakfast[2:5]             # ["bacon", "pancakes", "oatmeal"]
+breakfast[-2:]             # ["yogurt", "toast"]
+breakfast.append("juice")  # Adds "juice" to the breakfast list
+breakfast.pop()            # Returns "juice", modifying breakfast
+
+# You can even concatenate lists
+
+united_kingdom = ["England", "Wales"] + ["Scottland", "Northern Ireland"]
+# Returns: ["England", "Wales", "Scottland", "Northern Ireland"]
+
 
